3781fcc21c73c5c6785c1bf9445f2825627c7cc8
vicagasi <vicagasi@ut.utm.edu>
2021-11-11 17:43:26 -0600

    3rd commit: updated main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main(){
 
-  cout << "Hello World!" << endl;
+  int userInput;
+  cout << "What value would you like for 'n'? ";
+  cin >> userInput;
+  cout << sum(userInput) << endl;
   
+}
+
+int sum(int n){
+  int sum = 0;
+  for(int i = 1; i <= n; i++)
+    sum = sum + i;
+  return sum;
 }
